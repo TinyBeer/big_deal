c6475d9a1f6a7c2e2d2387d246ff755a4d0ca4d1
beer <beer@Beer.lan>
2025-09-01 22:31:05 +0800

    feat jdjr interactive game -- work with name

--- gogogo.js
+++ gogogo.js
@@ -18,10 +18,10 @@ console.log("已开启屏幕常亮");
 
 /* waiting until specific time */
 const waitTime = utils.getTimeToTarget(targetHour, targetMinute);
-utils.preciseSleep(waitTime, true);
+// utils.preciseSleep(waitTime, true);
 
 jdjr.run();
-jd.run();
+// jd.run();
 
 device.cancelKeepingAwake();
 console.log("已关闭屏幕常亮");
--- jdjr.js
+++ jdjr.js
@@ -2,67 +2,31 @@
 const jutils = require("./utils");
 
 /* config */
-
-const matrix = [
-  {
-    origin: {
-      x: 112,
-      y: 1200,
-    },
-    height: 250,
-    width: 1000,
-    // delayList: [[1, 1, 1, 1, 1]],
-    delayList: [[59, 34, 9, 14, 14]],
-  },
-  {
-    origin: {
-      x: 112,
-      y: 1500,
-    },
-    height: 500,
-    width: 1000,
-    delayList: [
-      // [1, 0, 0, 1],
-      [9, 0, 0, 29],
-      // [1, 1, 2, 1],
-      [14, 14, 58, 14],
-    ],
-  },
-  {
-    origin: {
-      x: 365,
-      y: 790,
-    },
-    scroll: true,
-    height: 1270,
-    width: 500,
-    delayList: [[15], [15], [15], [15], [15], [15], [15], [15], [15]],
-  },
+const objList = [
+  { name: "雀神来也", dur_m: 60 },
+  { name: "货柜趣消除", dur_m: 35 },
+  { name: "趣味叠叠乐", dur_m: 15 },
+  { name: "排队上车", dur_m: 15 },
+  { name: "方块拼图", dur_m: 15 },
+  { name: "养猪猪", dur_m: 10 },
+  { name: "京豆捕鱼", dur_m: 8 },
+  { name: "种菜领现金", dur_m: 0 },
+  { name: "消灭小萌星", dur_m: 30 },
+  { name: "麻将凑十", dur_m: 15 },
+  { name: "解压硬币", dur_m: 15 },
+  { name: "2048方块", dur_m: 60 },
+  { name: "毛线大师", dur_m: 15 },
+  { scroll: true },
+  { name: "点点2048", dur_m: 15 },
+  { name: "合成原始人", dur_m: 15 },
+  { name: "无尽泡泡龙", dur_m: 15 },
+  { name: "打螺丝王者", dur_m: 15 },
+  { name: "数字喜加1", dur_m: 15 },
+  { name: "纸牌接龙", dur_m: 15 },
+  { name: "喵喵十消", dur_m: 15 },
+  { name: "超级连连看", dur_m: 15 },
+  // { name: "2048", dur_m: 0.1 },
 ];
-
-// const matrix = [
-//   {
-//     origin: {
-//       x: 112,
-//       y: 1200,
-//     },
-//     height: 250,
-//     width: 1000,
-//     delayList: [[1, 1, 1, 1, 1]],
-//   },
-//   {
-//     origin: {
-//       x: 112,
-//       y: 1500,
-//     },
-//     height: 500,
-//     width: 1000,
-//     delayList: [
-//       [1, 0, 0, 1],
-//       [1, 1, 2, 1],
-//     ],
-//   },
-// ];
 
 function run() {
   // launch app
@@ -72,67 +36,56 @@ function run() {
   // enter interactive games
   enterInteractiveGames();
 
-  for (let a = 0; a < matrix.length; a++) {
-    var obj = matrix[a];
-    var dy = 0;
-    if (obj.delayList.length > 1) {
-      dy = obj.height / (obj.delayList.length - 1);
-    }
-    if (obj.scroll) {
-      scrollDown();
-      sleep(2000);
-      scrollDown();
-      sleep(2000);
-    }
-    for (let i = 0; i < obj.delayList.length; i++) {
-      var dx = 0;
-      if (obj.delayList[i].length > 1) {
-        dx = obj.width / (obj.delayList[i].length - 1);
-      }
-      for (let j = 0; j < obj.delayList[i].length; j++) {
-        sleep(2000);
-        var x = obj.origin.x + dx * j;
-        var y = obj.origin.y + dy * i;
-        var dur = obj.delayList[i][j] * 60 * 1000;
-        if (dur == 0) {
-          console.log("task time == " + dur + " skip task");
-          continue;
-        }
-        console.log(
-          "click task[" +
-            i +
-            "," +
-            j +
-            "] pos[" +
-            x +
-            "," +
-            y +
-            "] sleep[" +
-            dur +
-            "]"
-        );
-        for (; dur > 0; ) {
-          var t = 8 * 60 * 1000;
-          if (dur <= t) {
-            t = dur;
-            dur = 0;
-          } else {
-            dur = dur - t;
-          }
+  // workWithPos();
+  workWithName();
 
-          console.log("act click pos[" + x + "," + y + "] sleep[" + t + "]");
-          click(x, y);
-          jutils.preciseSleep(t, false);
-          back();
-          back();
-          sleep(2000);
-        }
-      }
-    }
-  }
-  back();
   back();
   sleep(2000);
+  back();
+  sleep(2000);
+}
+
+function workWithName() {
+  for (let i = 0; i < objList.length; i++) {
+    let e = objList[i];
+    if (e.scroll) {
+      scrollDown();
+      sleep(2000);
+      scrollDown();
+      sleep(2000);
+      continue;
+    }
+
+    console.log(`play [${e.name}] ${e.dur_m} minute`);
+    var dur = e.dur_m * 60 * 1000;
+    if (dur == 0) {
+      console.log("task time == " + dur + " skip task");
+      continue;
+    }
+    let tmp = text(e.name).findOne(1000);
+    if (!tmp) {
+      console.log(`cant not found [${e.name}], skip`);
+      continue;
+    }
+    let obj = tmp.parent();
+    console.log(` get [${e.name}] pos[${obj.center()}]`);
+    for (; dur > 0; ) {
+      var t = 8 * 60 * 1000;
+      if (dur <= t) {
+        t = dur;
+        dur = 0;
+      } else {
+        dur = dur - t;
+      }
+
+      console.log(`act play [${e.name}] ${t / 60 / 1000}min`);
+      click(obj.center());
+      jutils.preciseSleep(t, false);
+      back();
+      back();
+      sleep(2000);
+    }
+  }
 }
 
 /* enter interactive games */
