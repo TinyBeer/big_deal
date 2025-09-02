573c3870d2269e2dced3fc3de2b748f9cf3548bb
beer <beer@Beer.lan>
2025-09-02 22:24:01 +0800

    feat usefaul script

---
+++ smzdm.js
@@ -0,0 +1,19 @@
+app.launchApp("什么值得买");
+sleep(8000);
+
+if (id("dialog_home_close").exists()) {
+    id("dialog_home_close").
+    findOne().click();
+}
+
+id("iv_left").findOne().click();
+sleep(1000);
+id("ll_login_sign").findOne().click();
+
+sleep(3000);
+if (id("iv_close").exists()) {
+    id("iv_close").findOne().click();
+}
+sleep(1000);
+back();
+back();
---
+++ state_grid_online.js
@@ -0,0 +1,8 @@
+app.launchApp("网上国网");
+var signBtn=text("签到").findOne();
+sleep(4000);
+click(540,1552);
+sleep(8000);
+back();
+back();
+
