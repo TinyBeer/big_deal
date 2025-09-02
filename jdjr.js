db6ee2845129d9c7df796a1b2a37e49a3c9a764a
zhangyong <2565786256@qq.com>
2025-09-02 14:15:25 +0800

    fix scroll problem

--- jdjr.js
+++ jdjr.js
@@ -29,21 +29,8 @@ const jutils = require("./utils");
 // ];
 
 const objList = [
-  { name: "雀神来也", dur_m: 0 },
-  { name: "货柜趣消除", dur_m: 1 },
-  { name: "趣味叠叠乐", dur_m: 0 },
-  { name: "排队上车", dur_m: 1 },
-  { name: "方块拼图", dur_m: 1 },
-  { name: "养猪猪", dur_m: 10 },
-  { name: "京豆捕鱼", dur_m: 8 },
-  { name: "种菜领现金", dur_m: 0 },
-  { name: "消灭小萌星", dur_m: 30 },
-  { name: "麻将凑十", dur_m: 15 },
-  { name: "解压硬币", dur_m: 15 },
-  { name: "2048方块", dur_m: 60 },
-  { name: "毛线大师", dur_m: 15 },
   { scroll: true },
-  { name: "点点2048", dur_m: 15 },
+  { name: "点点2048", dur_m: 1 },
   { name: "合成原始人", dur_m: 15 },
   { name: "无尽泡泡龙", dur_m: 15 },
   { name: "打螺丝王者", dur_m: 15 },
@@ -72,7 +59,7 @@ function run() {
 }
 
 function workWithName() {
-  let scorlled = false;
+  var scorlled = false;
   for (let i = 0; i < objList.length; i++) {
     let e = objList[i];
     if (e.scroll) {
@@ -113,7 +100,10 @@ function workWithName() {
       back();
       sleep(2000);
 
-      open_box(scorlled);
+      if (!open_box(scorlled)) {
+        console.log("open box failed, break");
+        break;
+      }
     }
   }
 }
@@ -149,6 +139,12 @@ function open_box(scrolld) {
   }
   let box = textContains("个盲盒待开").findOne(1000);
   if (!box) {
+    if (scrolld) {
+      scrollDown();
+      sleep(1000);
+      scrollDown();
+      sleep(1000);
+    }
     return true;
   }
   console.log(box.text());
