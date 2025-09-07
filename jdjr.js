f9ae7314f03e7fb33f8e3d696bd256f8e0e6e4a3
beer <beer@Beer.lan>
2025-09-07 09:50:00 +0800

    opt jdjr interactive games -- auto gen tasks

--- jdjr.js
+++ jdjr.js
@@ -2,16 +2,16 @@
 const jutils = require("./utils");
 
 /* config */
-
+let nameList = ["雀神来也", "货柜趣消除", "趣味叠叠乐", "排队上车", "方块拼图"];
 let gameTaskList = [];
 
 let hotGameTaskList = [
   // round one
-  { name: "雀神来也", dur_m: 60 },
-  { name: "货柜趣消除", dur_m: 35 },
-  { name: "趣味叠叠乐", dur_m: 15 },
-  { name: "排队上车", dur_m: 15 },
-  { name: "方块拼图", dur_m: 15 },
+  // { name: "雀神来也", dur_m: 60 },
+  // { name: "货柜趣消除", dur_m: 35 },
+  // { name: "趣味叠叠乐", dur_m: 15 },
+  // { name: "排队上车", dur_m: 15 },
+  // { name: "方块拼图", dur_m: 15 },
   // { name: "养猪猪", dur_m: 4 },
   // { name: "京豆捕鱼", dur_m: 8 },
   // { name: "种菜领现金", dur_m: 0 },
@@ -21,20 +21,20 @@ let hotGameTaskList = [
   // { name: "2048方块", dur_m: 60 },
   // { name: "无尽战歌", dur_m: 15 },
   // round two
-  { name: "雀神来也", dur_m: 2 },
-  { name: "货柜趣消除", dur_m: 2 },
-  { name: "趣味叠叠乐", dur_m: 2 },
-  { name: "排队上车", dur_m: 2 },
-  { name: "方块拼图", dur_m: 2 },
+  // { name: "雀神来也", dur_m: 2 },
+  // { name: "货柜趣消除", dur_m: 2 },
+  // { name: "趣味叠叠乐", dur_m: 2 },
+  // { name: "排队上车", dur_m: 2 },
+  // { name: "方块拼图", dur_m: 2 },
   // { name: "养猪猪", dur_m: 5 },
-  // { name: "京豆捕鱼", dur_m: 0 },
-  // { name: "种菜领现金", dur_m: 0 },
-  // { name: "消灭小萌星", dur_m: 2 },
-  // { name: "麻将凑十", dur_m: 2 },
-  // { name: "解压硬币", dur_m: 2 },
-  // { name: "2048方块", dur_m: 2 },
-  // { name: "无尽战歌", dur_m: 15 },
-  // { name: "养猪猪", dur_m: 2 },
+  { name: "京豆捕鱼", dur_m: 0 },
+  { name: "种菜领现金", dur_m: 0 },
+  { name: "消灭小萌星", dur_m: 2 },
+  { name: "麻将凑十", dur_m: 2 },
+  { name: "解压硬币", dur_m: 2 },
+  { name: "2048方块", dur_m: 2 },
+  { name: "无尽战歌", dur_m: 2 },
+  { name: "养猪猪", dur_m: 2 },
 ];
 
 let moreGameTaskList = [
@@ -68,11 +68,15 @@ function run() {
 
   // enter interactive games
   enterInteractiveGames();
-  let tasks = getGameTaskList(getGameNameList("精品推荐", true));
-  gameTaskList = hotGameTaskList.concat(tasks, moreGameTaskList);
+  let gameNameList = getGameNameList("精品推荐", true);
+  nameList = nameList.concat(gameNameList);
+  console.log(nameList);
+  let tasks = getGameTaskList(nameList);
+  gameTaskList = tasks.concat(moreGameTaskList);
   console.log(gameTaskList);
-  // // workWithPos();
+  // workWithPos();
   workWithName(gameTaskList);
+  // workWithName(hotGameTaskList);
 
   back();
   sleep(2000);
@@ -233,7 +237,7 @@ function getGameTaskList(gameNameList) {
       } else {
         taskList.push({
           name: name,
-          dur_m: min - mins[j - 1],
+          dur_m: min - mins[j - 1] + 1,
         });
       }
     }
@@ -296,6 +300,8 @@ function getGameNameList(pivotName, ignorePivot) {
     if (
       view.text().length !== 0 &&
       !view.text().includes("q70") &&
+      !view.text().includes("+") &&
+      !view.text().includes("blindBox") &&
       !(view.text() === pivotName && ignorePivot) &&
       view.text() !== "种菜领现金"
     ) {
