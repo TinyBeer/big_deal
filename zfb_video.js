180383328983272d4b2a8aac75b1eea82278bab2
zhangyong <2565786256@qq.com>
2025-10-17 15:38:25 +0800

    feat zfb video

---
+++ zfb_video.js
@@ -0,0 +1,24 @@
+const x = 500;
+const sy = 1000;
+const ey = 300;
+const dur = 300;
+for (let idx = 0; idx < 50; idx++) {
+  // let r = random();
+  // console.log(r);
+  // let flag = r > 0.5;
+  let flag = true;
+  while (flag) {
+    flag = false;
+    swipe(x, sy, x, ey, dur);
+    sleep(5000);
+    let btn = textContains("进入直播间").findOne(500);
+    flag = btn != null;
+  }
+
+  let check = textContains("依次点击").findOne(500);
+  if (check) {
+    notice("支付宝浏览视频", "真人验证");
+    confirm("真人验证提示", "确认");
+  }
+  sleep(10000);
+}
