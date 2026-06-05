8880ac046ea05f20c82cb29792b30ebf586c5363
zhangyong <2565786256@qq.com>
2026-06-05 13:34:28 +0800

    feat(jdjr) 互动游戏更新

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
-console.log(context.getResources().getDisplayMetrics());
-// var name = "新奇集市";
-// let enter = className("android.widget.TextView")
-//   .text(name)
-//   .depth(23)
-//   .findOne(200);
-// console.log(enter);
+let obj = text("养猪猪").findOne(100).parent().parent().parent();
+displayArchitecture(obj, 1)
+
+function displayArchitecture_Name(name) {
+  let root = text(name).findOne(100);
+  console.log(root);
+  
+  if (root == null) {
+    return;
+  }
+
+  displayArchitecture(root, 1);
+}
+
+function displayArchitecture(root, depth) {
+  console.log("->".repeat(depth),"id:", root.id(), "text:", root.text(), "desc:", root.desc(), "ccount:", root.childCount(), "bounds:", root.bounds());
+  for (let idx = 0; idx < root.children().length; idx++) {
+    let child = root.children()[idx];
+    displayArchitecture(child, depth + 1);
+  }
+}
